d8b37222d61fd3c1ec110c6265342a7e9ac7bd90
Raí Soares <raisoares.dev@gmail.com>
2023-09-22 21:55:55 -0300

    GUI added

--- src/app.py
+++ src/app.py
@@ -1,14 +1,36 @@
 from RiemannSum import RiemannSum
 from sympy import parse_expr
+from PySimpleGUI import PySimpleGUI as gui
 
-min_value = float(input("Informe o valor de a: "))
-max_value = float(input("Informe o valor de b: "))
-n = int(input("Informe o número de intervalos: "))
-exp = parse_expr(input("Informe o f(x): "))
-dx = (max_value - min_value)/n
+gui.theme("Reddit")
+layout = [
+  [gui.Text("Informe o valor de a: "), gui.Input(key="min", size=(10, 1))],
+  [gui.Text("Informe o valor de b: "), gui.Input(key="max", size=(10, 1))],
+  [gui.Text("Informe o número de intervalos: "), gui.Input(key="n", size=(10, 1))],
+  [gui.Text("Informe o f(x): "),gui.Input(key="fx", size=(20, 1))],
+  [gui.Button("Calcular")],
+]
 
-riemannSum = RiemannSum(min_value, max_value, n, exp)
+window = gui.Window("Soma de Reimann", layout)
 
-print("Soma de Riemann à esquerda:", riemannSum.resultLeft())
-print("Soma de Riemann à direita:", riemannSum.resultRight())
-print("O resultado da integral definida é:", riemannSum.resultIntegral())
+while True:
+    event, values = window.read()
+    if event == gui.WIN_CLOSED:
+        break
+    if event == "Calcular":
+        try:
+            min_value = float(values["min"])
+            max_value = float(values["max"])
+            n = int(values["n"])
+            exp = parse_expr(values["fx"])
+
+            riemannSum = RiemannSum(min_value, max_value, n, exp)
+
+            gui.popup("Soma de Riemann à esquerda: " + str(riemannSum.resultLeft()),
+                     "Soma de Riemann à direita: " + str(riemannSum.resultRight()),
+                     "O resultado da integral definida é: " + str(riemannSum.resultIntegral()))
+
+        except Exception as e:
+            gui.popup("Erro: Certifique-se de que os valores estão corretos.")
+            
+window.close()
